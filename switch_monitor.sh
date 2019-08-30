6f37c4ffc69b9b89b59abd3680ad19a66a17d04f
YoonYeoSong <yeosong92@gmail.com>
2019-08-30 18:11:43 +0900

    swich modified

--- switch_monitor.sh
+++ switch_monitor.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 L="$(xrandr -q | grep 'HDMI-1-1')"
-R="$(xrandr -q | grep 'DP-1-2')"
+R="$(xrandr -q | grep ddddddd'DP-1-2')"
 
+
+sdfksdjflskdfsdkfsdksksdmfksdf
+sdfsfsdfsdfsdfsdfsdfsdfdsfdsf
 L_ON=[[ $L = *"connected"* ]]
 R_ON=[[ $R = *"connected"* ]]
 
