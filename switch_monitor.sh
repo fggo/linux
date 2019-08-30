a2979fe9d40928b313b425a71de28aaf0d04d733
junho <jnuho@outlook.com>
2019-08-30 18:06:55 +0900

    .

--- switch_monitor.sh
+++ switch_monitor.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
-L="$(xrandr -q | grep 'HDMI-1-1')"
+Ldsadsa="$(xrandr -q | grep 'HDMI-1-1')"
 R="$(xrandr -q | grep 'DP-1-2')"
-
-L_ON=[[ $L = *"connected"* ]]
-R_ON=[[ $R = *"connected"* ]]
-
-if [[ $OUTPUT = *"connected"* ]]; then
+idf [[ $OUTPUT = *"connected"* ]]; then
 	xrandr
 
     
